281d2c798f881e31b889e9058ae74a5277350fb5
zalba monteiro <rosalbasmonteiro@gmail.com>
2021-12-01 21:34:24 -0300

    primeiro commit com exercicio 1 e 3

---
+++ src/components/ConcatString.vue
@@ -0,0 +1,50 @@
+<template>
+  <b-container class="mt-5">
+    <b-row align-h="center" >
+      <b-col  sm="6">
+        <b-form-group label="Nome :">
+          <b-form-input placeholder="Nome" v-model="nome" />
+        </b-form-group>
+        <b-form-group class="mt-3" label="Sobrenome :">
+          <b-form-input placeholder="Sobrenome" v-model="sobrenome" />
+        </b-form-group>
+      </b-col>
+    </b-row>
+    <b-row class="justify-content-sm-center">
+      <b-col sm='2' align-self="center" class="mt-3">
+        <b-button variant="primary" class="center-button" @click="concatena">Concater</b-button>
+      </b-col>
+    </b-row>
+    <b-row class="mt-3">
+      <p>{{resultado}}</p>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+export default {
+  name: "ConcatString",
+  data: function () {
+    return {
+      nome: "",
+      sobrenome: "",
+      resultado : ""
+    };
+  },
+  methods : {
+    concatena : function(){
+      this.resultado = this.nome + " " + this.sobrenome
+    }
+  }
+};
+</script>
+<style>
+.box-content {
+  padding-top: 100px;
+}
+
+.center-button{
+  margin: auto;
+  display: block!important;
+}
+</style>
---
+++ src/components/ToDoList.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <b-container>
+      <b-row class="mt-5">
+        <b-col sm="12">
+          <h2 class="text-center">To do list</h2>
+        </b-col>
+      </b-row>
+      <b-row align-h="center">
+        <b-col sm="6">
+          <b-input-group>
+            <b-input placeholder="Insira seu texto" v-model="novoItem" />
+            <b-input-group-append>
+              <b-button variant="primary" @click="adicionarItem"> Adicionar</b-button>
+            </b-input-group-append>
+          </b-input-group>
+        </b-col>
+      </b-row>
+      <b-row align-h="center" class="mt-5">
+        <b-col sm="6">
+          <ul>
+            <li v-for="(umIntem, index) in itens" :key="index">
+              {{umIntem}}
+              <b-button variant="danger" @click="removeItem(index)"> Deletar </b-button>
+            </li>
+          </ul>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+<script>
+export default {
+  name: "ToDoList",
+  data: function(){
+    return {
+      novoItem : "",
+      itens : []
+    }
+  },
+  methods:{
+    adicionarItem : function(){
+      this.itens.push(this.novoItem)
+    },
+    removeItem : function(index){
+      const array = this.itens;
+      this.itens = array.splice(index, 1)
+    }
+  }
+}
+</script>
+<style>
+  .text-center{
+    text-align: center;
+  }
+</style>
